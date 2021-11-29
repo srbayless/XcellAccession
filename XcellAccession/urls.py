6f0c831bb937a92748985a078f000122b042eba1
Frederico Alves <frederico.miranda.brandao@gmail.com>
2021-11-29 16:14:49 -0500

    Refactor, assigning each view to a variable.

--- XcellAccession/urls.py
+++ XcellAccession/urls.py
@@ -21,15 +21,25 @@ from authentication import views as auth_views
 from web_app import views
 from shared import names, routes
 
+VIEW_ACCESSION = views.AccessionView
+VIEW_ADMIN = admin.site.urls
+VIEW_DASHBOARD = views.IndexView
+VIEW_FSFORM = views.TestingView
+VIEW_LOGIN = auth_views.LoginView.as_view()
+VIEW_LOGOUT = auth_views.LogoutView
+VIEW_REPORTS = views.ReportView
+VIEW_REVIEW = views.editView
+VIEW_SIGNUP = auth_views.SignUpView
+
 urlpatterns = [
-    path(routes.ROUTE_ADMIN, admin.site.urls, name=names.NAME_ADMIN),
-    path(routes.ROUTE_DASHBOARD, views.IndexView, name=names.NAME_DASHBOARD),
-    path(routes.ROUTE_SIGNUP, auth_views.SignUpView, name=names.NAME_SIGNUP),
-    path(routes.ROUTE_LOGIN, auth_views.LoginView.as_view(), name=names.NAME_LOGIN),
-    path(routes.ROUTE_LOGOUT, auth_views.LogoutView, name=names.NAME_LOGOUT),
-    path(routes.ROUTE_ACCESSION, views.AccessionView, name=names.NAME_ACCESSION),
-    path(routes.ROUTE_REVIEW, views.editView, name=names.NAME_REVIEW),
-    path(routes.ROUTE_FSFORM, views.TestingView, name=names.NAME_TESTING),
-    path(routes.ROUTE_REPORTS, views.ReportView, name=names.NAME_REPORTS),
+    path(routes.ROUTE_ADMIN, VIEW_ADMIN, name=names.NAME_ADMIN),
+    path(routes.ROUTE_DASHBOARD, VIEW_DASHBOARD, name=names.NAME_DASHBOARD),
+    path(routes.ROUTE_SIGNUP, VIEW_SIGNUP, name=names.NAME_SIGNUP),
+    path(routes.ROUTE_LOGIN, VIEW_LOGIN, name=names.NAME_LOGIN),
+    path(routes.ROUTE_LOGOUT, VIEW_LOGOUT, name=names.NAME_LOGOUT),
+    path(routes.ROUTE_ACCESSION, VIEW_ACCESSION, name=names.NAME_ACCESSION),
+    path(routes.ROUTE_REVIEW, VIEW_REVIEW, name=names.NAME_REVIEW),
+    path(routes.ROUTE_FSFORM, VIEW_FSFORM, name=names.NAME_FSFORM),
+    path(routes.ROUTE_REPORTS, VIEW_REPORTS, name=names.NAME_REPORTS),
     url(r'^search/', include(('search.urls', 'search'), namespace='search')),
 ]
