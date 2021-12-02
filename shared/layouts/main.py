45d318492f663053098c2193546b6d6ad50c34fd
Frederico Alves <frederico.miranda.brandao@gmail.com>
2021-12-02 08:37:34 -0500

    Add link for follow-ups on half up of the header.

--- shared/layouts/main.py
+++ shared/layouts/main.py
@@ -11,13 +11,17 @@ DEFAULT_STYLESHEETS = [
 ]
 
 DEFAULT_CONTEXT_CONTENT = {}
+DEFAULT_CONTEXT_FOLLOW_UPS = {
+    'count_pending': 0,
+}
 
 DEFAULT_CONTEXT_MAIN = {
     'page_title': 'Page Title',
     'links_stylesheet': DEFAULT_STYLESHEETS,
     'template_header': templates.TEMPLATE_HEADER,
     'template_content': templates.TEMPLATE_DEFAULT,
-    'context_content': DEFAULT_CONTEXT_CONTENT,    
+    'context_content': DEFAULT_CONTEXT_CONTENT,
+    'context_follow_ups': DEFAULT_CONTEXT_FOLLOW_UPS,
 }
 
 def GENERATE_DEFAULT_CONTEXT():
