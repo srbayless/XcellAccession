22cd6e6ada568c73fdff601356a3866d70702a0d
Frederico Alves <frederico.miranda.brandao@gmail.com>
2021-12-01 19:08:25 -0500

    Refactor layout so that it resembles more the one demonstrated by the client.

--- shared/layouts/main.py
+++ shared/layouts/main.py
@@ -1,4 +1,4 @@
-from shared import names, routes
+from shared import names, routes, templates
 
 # default values used by the main layout
 
@@ -8,35 +8,12 @@ DEFAULT_STYLESHEETS = [
     'assets/css/main.css',
 ]
 
-DEFAULT_CONTEXT_LOGO = {}
-DEFAULT_CONTEXT_USER = {}
-DEFAULT_CONTEXT_MENU = {
-    'navigation_links': {
-        names.NAME_ACCESSION: {
-            'label': 'ACCESSIONING',
-            'link': routes.ROUTE_ACCESSION,
-        },
-        names.NAME_REPORTS: {
-            'label': 'REPORTS',
-            'link': routes.ROUTE_REPORTS,
-        },
-        names.NAME_LOGOUT: {
-            'label': 'LOG OUT',
-            'link': routes.ROUTE_LOGOUT,
-        }
-    },
-}
 DEFAULT_CONTEXT_CONTENT = {}
 
 DEFAULT_CONTEXT_MAIN = {
     'page_title': 'Page Title',
     'links_stylesheet': DEFAULT_STYLESHEETS,
-    'template_logo': 'layouts/main/logo.html.part',
-    'template_user': 'layouts/main/user.html.part',
-    'template_menu': 'layouts/main/menu.html.part',
-    'template_content': 'layouts/main/contents/default.html.part',
-    'context_logo': DEFAULT_CONTEXT_LOGO,
-    'context_user': DEFAULT_CONTEXT_USER,
-    'context_menu': DEFAULT_CONTEXT_MENU,
+    'template_header': templates.TEMPLATE_HEADER,
+    'template_content': templates.TEMPLATE_DEFAULT,
     'context_content': DEFAULT_CONTEXT_CONTENT,    
 }
---
+++ shared/templates.py
@@ -0,0 +1,3 @@
+TEMPLATE_ACCESSION = 'layouts/main/contents/accession.html.part'
+TEMPLATE_DEFAULT = 'layouts/main/contents/default.html.part'
+TEMPLATE_HEADER = 'layouts/main/header/index.html.part'
